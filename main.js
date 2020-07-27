b764d78eb1b2b7004f3684c828dc8e3adf441748
Sharod Bailey <Sharod.Bailey@codeimmersives.comy>
2020-07-27 08:39:58 -0400

    not done

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+const fs = require('fs');
+const files = fs.readFileSync('./students.json', 'utf8');
+const students = JSON.parse(files)
 
+const getStudentByIndex = (anIndex) => {
+  return students.students[anIndex];
+}
 
-
-
-
-
+const getStudentByName = (aName) => {
+  for (const item of students.students) {
+    if (item.name === aName) {
+      return item;
+    }
+  }
+  return -1;
+}
 
 if (typeof getStudentByIndex === 'undefined') {
   getStudentByIndex = undefined;
